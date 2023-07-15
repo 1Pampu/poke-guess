b963efafc406c1f71433edb62448300cee0f8010
1Pampu <piampianimartin@gmail.com>
2023-07-14 23:23:51 -0300

    autocomplete v1

--- src/scripts.js
+++ src/scripts.js
@@ -1,41 +1,86 @@
-var apiResponse = ""
+// Global variables and requests
+var apiRandom = "";
+var apiPdex = "";
+var pkmonList = "";
+newRandom();
+getPdex();
 
-function mostrarContenido() {
+// Functions
+function showContent() {
   var boton = document.getElementById("startButton");
   boton.style.display = "none"; // Show start button
 
-  var contenido = document.getElementById("content");
-  contenido.style.display = "block"; // Show page content
+  var content = document.getElementById("content");
+  content.style.display = "block"; // Show page content
+}
 
-  // Send Request
+function newRandom() {
   fetch("https://my-poke-api.onrender.com/random")
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      apiResponse = data
+      apiRandom = data;
     })
 }
 
-// Evento que se activa cuando el contenido del campo de texto cambia
-document.getElementById("autocomplete-input").addEventListener("input", function () {
-  var resultsDiv = document.getElementById("autocomplete-results")
-  var inputText = this.value; // Actual value in the box
-
-  // Send Request
-  fetch("https://my-poke-api.onrender.com/pokemon/?name=" + inputText)
+function getPdex() {
+  fetch("https://my-poke-api.onrender.com/")
     .then(function (response) {
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw new Error("Error in the API request", response.status);
-      }
+      return response.json();
     })
     .then(function (data) {
-      data.forEach(pokemon => {
-        var p = document.createElement("p");
-        p.textContent = pokemon["name"];
-        resultsDiv.appendChild(p);
+      apiPdex = data;
+      pkmonList = data.map(function (jsonObj) {
+        return jsonObj.name;
       });
     })
-});
+}
+
+// Event Listeners
+document.getElementById("autocomplete-input").addEventListener("input", function () {
+  var input = this.value;
+  var resultsDiv = document.getElementById("autocomplete-results");
+  resultsDiv.innerHTML = "";
+
+  if (input.length > 0) {
+    var matchingPkmons = pkmonList.filter(function (pokemon) {
+      return pokemon.toLowerCase().startsWith(input.toLowerCase());
+    });
+    matchingPkmons.forEach(pokemon => {
+      var p = document.createElement("p");
+      p.textContent = pokemon;
+      resultsDiv.appendChild(p);
+    })
+  }
+})
+
+
+// // Evento que se activa cuando el contenido del campo de texto cambia
+// document.getElementById("autocomplete-input").addEventListener("input", function () {
+//   var resultsDiv = document.getElementById("autocomplete-results")
+//   var inputText = this.value; // Actual value in the box
+//   resultsDiv.innerHTML = ""  // Remove previus results
+
+//   // Send Request
+//    fetch("https://my-poke-api.onrender.com/pokemon/?name=" + inputText)
+//   //fetch("http://127.0.0.1:8000/pokemon/?name=" + inputText)
+//     .then(function (response) {
+//       if (response.ok) {
+//         return response.json();
+//       }
+//       else {
+//         throw new Error("Pokemon not Found!");
+//       }
+//     })
+//     .then(function (data) {
+//       data.forEach(pokemon => {
+//         var p = document.createElement("p");
+//         p.textContent = pokemon["name"];
+//         resultsDiv.appendChild(p);
+//       });
+//     })
+//     .catch(function (error) {
+//       console.log("Error:", error);
+//     });
+// })
