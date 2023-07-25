83b0cc5400954a104872cf1e53755fd9d2ff1769
1Pampu <piampianimartin@gmail.com>
2023-07-24 21:28:38 -0300

    remove selected pokemons

--- src/scripts.js
+++ src/scripts.js
@@ -79,81 +79,88 @@ function postRequest() {
   // Function to submit the post request to the server
   pNum = pdexNumberInput();
   if (pNum != null) {
-    var data = { "pokedex_number": pdexNumberInput() }
-    fetch("https://my-poke-api.onrender.com/answer", { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify(data) })
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        var pokemon = getPokemonDictionary(pNum, apiPdex);
-        var divContainer = document.createElement("div");
-        divContainer.classList.add("my-answer");
+    var pokemon = getPokemonDictionary(pNum, apiPdex);
+    if (pkmonList.includes(pokemon.name))
+      var data = { "pokedex_number": pdexNumberInput() }
+      fetch("https://my-poke-api.onrender.com/answer", { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify(data) })
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          var divContainer = document.createElement("div");
+          divContainer.classList.add("my-answer");
 
-        // Pokemon Image
-        var divImage = document.createElement("div");
-        divImage.classList.add("box");
-        var img = document.createElement("img");
-        img.src = "data:image/png;base64," + pokemon.image; // Decode image
-        divImage.appendChild(img);
-        divContainer.appendChild(divImage);
+          // Pokemon Image
+          var divImage = document.createElement("div");
+          divImage.classList.add("box");
+          var img = document.createElement("img");
+          img.src = "data:image/png;base64," + pokemon.image; // Decode image
+          divImage.appendChild(img);
+          divContainer.appendChild(divImage);
 
-        // Pokedex Number
-        var divPkdexnumber = document.createElement("div");
-        divPkdexnumber.classList.add("box", data.pokedex_number);
-        var pPkdexnumber = document.createElement("p");
-        pPkdexnumber.textContent = pokemon.pokedex_number;
-        divPkdexnumber.appendChild(pPkdexnumber);
-        divContainer.appendChild(divPkdexnumber);
+          // Pokedex Number
+          var divPkdexnumber = document.createElement("div");
+          divPkdexnumber.classList.add("box", data.pokedex_number);
+          var pPkdexnumber = document.createElement("p");
+          pPkdexnumber.textContent = pokemon.pokedex_number;
+          divPkdexnumber.appendChild(pPkdexnumber);
+          divContainer.appendChild(divPkdexnumber);
 
-        // Type 1
-        var divType1 = document.createElement("div");
-        divType1.classList.add("box", data.type1);
-        var pType1 = document.createElement("p");
-        pType1.textContent = capitalize(pokemon.type1);
-        divType1.appendChild(pType1);
-        divContainer.appendChild(divType1);
+          // Type 1
+          var divType1 = document.createElement("div");
+          divType1.classList.add("box", data.type1);
+          var pType1 = document.createElement("p");
+          pType1.textContent = capitalize(pokemon.type1);
+          divType1.appendChild(pType1);
+          divContainer.appendChild(divType1);
 
-        // Type 2
-        var divType2 = document.createElement("div");
-        divType2.classList.add("box", data.type2);
-        var pType2 = document.createElement("p");
-        pType2.textContent = capitalize(pokemon.type2);
-        divType2.appendChild(pType2);
-        divContainer.appendChild(divType2);
+          // Type 2
+          var divType2 = document.createElement("div");
+          divType2.classList.add("box", data.type2);
+          var pType2 = document.createElement("p");
+          pType2.textContent = capitalize(pokemon.type2);
+          divType2.appendChild(pType2);
+          divContainer.appendChild(divType2);
 
-        // Height
-        var divHeight = document.createElement("div");
-        divHeight.classList.add("box", data.height_m);
-        var pHeight = document.createElement("p");
-        pHeight.textContent = pokemon.height_m + "m";
-        divHeight.appendChild(pHeight);
-        divContainer.appendChild(divHeight);
+          // Height
+          var divHeight = document.createElement("div");
+          divHeight.classList.add("box", data.height_m);
+          var pHeight = document.createElement("p");
+          pHeight.textContent = pokemon.height_m + "m";
+          divHeight.appendChild(pHeight);
+          divContainer.appendChild(divHeight);
 
-        // Weight
-        var divWeight = document.createElement("div");
-        divWeight.classList.add("box", data.weight_kg);
-        var pWeight = document.createElement("p");
-        pWeight.textContent = pokemon.weight_kg + "Kg";
-        divWeight.appendChild(pWeight);
-        divContainer.appendChild(divWeight);
+          // Weight
+          var divWeight = document.createElement("div");
+          divWeight.classList.add("box", data.weight_kg);
+          var pWeight = document.createElement("p");
+          pWeight.textContent = pokemon.weight_kg + "Kg";
+          divWeight.appendChild(pWeight);
+          divContainer.appendChild(divWeight);
 
-        // Legendary
-        var divLegendary = document.createElement("div");
-        divLegendary.classList.add("box", data.is_legendary);
-        var pLegendary = document.createElement("p");
-        if (pokemon.is_legendary == 0) { pLegendary.textContent = "No" }
-        else { pLegendary.textContent = "Yes" }
-        divLegendary.appendChild(pLegendary);
-        divContainer.appendChild(divLegendary);
+          // Legendary
+          var divLegendary = document.createElement("div");
+          divLegendary.classList.add("box", data.is_legendary);
+          var pLegendary = document.createElement("p");
+          if (pokemon.is_legendary == 0) { pLegendary.textContent = "No" }
+          else { pLegendary.textContent = "Yes" }
+          divLegendary.appendChild(pLegendary);
+          divContainer.appendChild(divLegendary);
 
-        // Append all
-        var answersDiv = document.getElementById("answers");
-        answersDiv.appendChild(divContainer);
+          // Append all
+          var answersDiv = document.getElementById("answers");
+          answersDiv.appendChild(divContainer);
 
-        // Remove input
-        var input = document.getElementById("autocomplete-input");
-        input.value = "";
-      })
+          // Remove input
+          var input = document.getElementById("autocomplete-input");
+          input.value = "";
+
+          // Remove pokemon from list & dictionary
+          var index = pkmonList.indexOf(pokemon.name);
+          pkmonList.splice(index,1);
+          index = apiPdex.findIndex(item => item.name === pokemon.name);
+          apiPdex.splice(index,1);
+        })
   }
 }
 
@@ -251,4 +258,4 @@ document.addEventListener("DOMContentLoaded", () => {
     })
 });
 
-// TODO: REMOVE SELECTED POKEMONS, END WHEN YOU GOT THE RIGHT ANSWER
+// TODO: END WHEN YOU GOT THE RIGHT ANSWER
